7ab094859b1b82a007f2b70cfb0992646cb3a43b
marcos-dev-web <marcospuglliesyprogramer@gmail.com>
2021-03-01 16:01:14 -0300

    added matriz to number

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 class Cube {
   constructor() {
-    this.element = "";
+    this.element = HTMLDivElement;
     this.gameBoard = document.querySelector(".game_board");
   }
 
@@ -20,11 +20,19 @@ class Cube {
 
   render(time = 1) {
     setTimeout(() => {
+      this.element.onmouseenter = (e) => {
+        e.target.style.transform += 'scale(1.2)'
+      }
+      this.element.onmouseout = (e) => {
+        e.target.style.transform = 'rotateZ(0)'
+      }
       this.gameBoard.appendChild(this.element);
     }, 100 * time);
     setTimeout(() => {
       this.element.style.transform = "rotateZ(0)";
     }, 150 * time);
+
+    return this.element;
   }
 }
 
@@ -63,10 +71,27 @@ function randomTwoNumbers(max) {
 
 
 function run() {
+  const gameBoard = document.querySelector(".game_board");
+  gameBoard.innerHTML = '';
+
+  let matriz = [
+    [null, null, null, null],
+    [null, null, null, null],
+    [null, null, null, null],
+    [null, null, null, null],
+  ]
 
   const numbersArray = randomTwoNumbers(16)
+  let x = 0;
+  let y = 0;
 
   for (let i = 0; i < numbersArray.length; i++) {
+
+    matriz[x][y] = numbersArray[i];
+
+    y = (y < 3) ? ++y : 0;
+    x = (y == 0) ? ++x : x;
+
     const cube = new Cube();
     cube.create(numbersArray[i]);
     cube.render(i);

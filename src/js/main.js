925f46a72086d530d4c03ca8f3fdf8c20e6fe1ab
marcos-dev-web <marcospuglliesyprogramer@gmail.com>
2021-03-01 20:39:08 -0300

    added animation

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,55 @@
 import Cube from './Cube.js';
 import { randomTwoNumbers } from './functions.js';
 import { event } from './listener.js';
+import Actions from './Actions.js';
+
 
 window.onload = () => {
   setTimeout(() => {
     run();
   }, 500);
 };
+
+function animation(elements, matriz) {
+  let interval;
+  let i =  0;
+  let x = 0;
+  let y = 0;
+
+  const action = new Actions(matriz);
+  interval = setInterval(() => {
+    let el = elements[i];
+    el.style = 'pointer-events: none;'
+
+    action.mark(el, el.children[0], [x, y], '#c56cf0');
+    y = y < 3 ? ++y : 0;
+    x = y == 0 ? ++x : x;
+    if (i == 15) {
+      clearInterval(interval);
+    }
+    i++;
+  }, 200);
+}
+
+function resetAnimation(elements, matriz) {
+  let interval;
+  let i =  0;
+  let x = 0;
+  let y = 0;
+
+  const action = new Actions(matriz);
+  interval = setInterval(() => {
+    let el = elements[i];
+
+    action.reset(el, el.children[0])
+    y = y < 3 ? ++y : 0;
+    x = y == 0 ? ++x : x;
+    if (i == 15) {
+      clearInterval(interval);
+    }
+    i++;
+  }, 200);
+}
 
 function run() {
   const gameBoard = document.querySelector(".game_board");
@@ -35,6 +78,7 @@ function run() {
     el.setAttribute('data-x', x);
     el.setAttribute('data-y', y);
 
+    el.style.pointerEvents = 'none';
     el.addEventListener("click", (e) => {
       event(e, matriz, () => {
         if (++countCorrect == 8) {
@@ -48,5 +92,15 @@ function run() {
 
     y = y < 3 ? ++y : 0;
     x = y == 0 ? ++x : x;
-  }
+  } // for
+
+  let timer;
+  setTimeout(() => {
+    var elements = [...document.querySelectorAll('.game_cube').values()];
+    animation(elements, matriz);
+    timer = setTimeout(() => {
+      resetAnimation(elements, matriz);
+    }, 200 * 16);
+  }, 1000)
+
 }
